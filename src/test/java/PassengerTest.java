7d7d7db2fc62ac8762ae0bda4bb2d0c1401c0b16
meeeiiike <g00407095@atu.ie>
2025-03-27 11:11:53 +0000

    added parametrized test along with created titleSuccess test case with more implemented in passenger class

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,6 +1,8 @@
 import ie.atu.week9.Passenger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,4 +20,18 @@ public class PassengerTest {
         assertEquals("Meike", passenger.getFirstName());
         assertEquals("e Meike", passenger.getLastName());
     }
+    @Test
+    void testTitleSuccess() {
+        assertEquals("Mr", passenger.getTitle());
+        Passenger passenger1 = new Passenger("Ms", "Meike", "e Meike");
+        assertEquals("Ms", passenger1.getTitle());
+        Passenger passenger2 = new Passenger("Mrs", "Meike", "e Meike");
+        assertEquals("Mrs", passenger2.getTitle());
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"Mr", "Mrs", "Ms"})
+    void testTitleSuccessAlso(String title) {
+        Passenger passenger3 = new Passenger(title, "Meike", "e Meike");
+    }
+
 }
